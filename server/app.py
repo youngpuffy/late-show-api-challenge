42d5a8be71365721dfb158de20ad53c00bc6ff3a
Youngpuffy <levimuturi2@gmail.com>
2025-06-23 09:56:05 +0300

    creates app.py

---
+++ server/app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_jwt_extended import JWTManager
+from server.models import db
+from server.config import Config
+from server.controllers.auth_controller import auth_bp
+from server.controllers.episode_controller import episode_bp
+from server.controllers.guest_controller import guest_bp
+from server.controllers.appearance_controller import appearance_bp
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+
+    db.init_app(app)
+    JWTManager(app)
+    
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(episode_bp)
+    app.register_blueprint(guest_bp)
+    app.register_blueprint(appearance_bp)
+    
+    with app.app_context():
+        db.create_all
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
