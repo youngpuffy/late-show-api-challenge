628de4963aee3b7f9fbf6081fba9209bb07c437c
Youngpuffy <levimuturi2@gmail.com>
2025-06-23 09:37:19 +0300

    adds episode_controller.py

--- server/controllers/episode_controller.py
+++ server/controllers/episode_controller.py
@@ -1,2 +1,35 @@
 from flask import Blueprint, jsonify, request
-from flasl_jwt_extende
+from flasl_jwt_extended import jwt_required
+from server.models import db
+from server.models.episode import Episode
+
+episode_bp = Blueprint('episode', __name__)
+
+@episode_bp.route('/episodes', methods=['GET'])
+def list_episodes():
+    episodes = Episode.query.all()
+    return jsonify ([{'id': e.id, 'number': e.number, 'date':e.date} for e in episodes])
+
+@episode_bp.route('/episodes/<int:id>', methods=['GET'])
+def get_episode(id):
+    episode = Episode.query.get_or_404(id)
+    return jsonify({
+        'id': episode.id,
+        'number': episode.number,
+        'date':episode.date,
+        'appearances':[
+            {
+                'id': a.id,
+                'guest_id': a.guest_id,
+                'guest_name': a.guest.name,
+                'rating': a.rating
+            } for a in episode.appearances
+        ]
+    })
+@episode_bp.route('/episodes/<int:id>', methods=['DELETE'])
+@jwt_required()
+def delete_episode(id):
+    episode = Episode.query.get_or_404(id)
+    db.session.delete(episode)
+    db.session.commit()
+    return jsonify(message='Episode and appearance deleted'), 200
